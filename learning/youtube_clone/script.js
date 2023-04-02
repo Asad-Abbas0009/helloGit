0e8052997e0792eef99c16c0e4923ce6b9ddb02b
Sikari16 <asadabbas4338@gmail.com>
2023-04-02 21:07:12 +0530

    add javascript to youtube clone

---
+++ learning/youtube_clone/script.js
@@ -0,0 +1,6 @@
+var menuTcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
